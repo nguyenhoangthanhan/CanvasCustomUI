a12f14228cf3d64446e40ad0eea6bb4441b7c5c2
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-16 18:42:22 +0700

    Implemented clockwise UI

--- CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/LiveClockScreen.kt
+++ CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/LiveClockScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,21 +20,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.rotate
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nguyenhoangthanhan.canvascustomui.navigation.CanvasCustomUIRouter
 import com.nguyenhoangthanhan.canvascustomui.navigation.Screen
 import com.nguyenhoangthanhan.canvascustomui.navigation.SystemBackButtonHandler
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.darkGray
+import com.nguyenhoangthanhan.canvascustomui.ui.theme.gray
+import com.nguyenhoangthanhan.canvascustomui.ui.theme.redOrange
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.white
+import java.lang.Math.PI
 import java.util.Calendar
 import java.util.Date
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun LiveClockScreen() {
     Box(
         modifier = Modifier
-            .fillMaxSize()
-            .background(white),
+            .background(white)
+            .fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
 
@@ -42,14 +51,20 @@ fun LiveClockScreen() {
             time = {
                 System.currentTimeMillis()
             },
-            circleRadius =,
-            outerCircleThickness =
+            circleRadius = 300f,
+            outerCircleThickness = 25f
         )
     }
 
     SystemBackButtonHandler {
         CanvasCustomUIRouter.navigateTo(Screen.HomeScreen)
     }
+}
+
+@Composable
+@Preview
+fun PreviewLiveClockScreen() {
+    LiveClockScreen()
 }
 
 @Composable
@@ -106,7 +121,118 @@ fun Clock(
                     radius = circleRadius,
                     center = circleCenter
                 )
+                drawCircle(
+                    color = gray,
+                    radius = 15f,
+                    center = circleCenter
+                )
+
+                val littleLineLength = circleRadius * 0.1f
+                val largeLineLength = circleRadius * 0.2f
+
+                for (i in 0 until 60) {
+                    val angleInDegrees = i * 360f / 60
+                    val angleInRad = angleInDegrees * PI / 180f + PI / 2f
+                    val lineLength = if (i % 5 == 0) {
+                        largeLineLength
+                    } else {
+                        littleLineLength
+                    }
+                    val lineThickness = if (i % 5 == 0) 5f else 2f
+
+                    val start = Offset(
+                        x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
+                        y = (circleRadius * sin(angleInRad) + circleCenter.y).toFloat()
+                    )
+                    val end = Offset(
+                        x = (circleRadius * cos(angleInRad) + circleCenter.x).toFloat(),
+                        y = (circleRadius * sin(angleInRad) + lineLength + circleCenter.y).toFloat()
+                    )
+                    rotate(
+                        angleInDegrees + 180,
+                        pivot = start,
+                        block = {
+                            drawLine(
+                                color = gray,
+                                start = start,
+                                end = end,
+                                strokeWidth = lineThickness.dp.toPx()
+                            )
+                        }
+                    )
+                }
+                val clockHands = listOf(ClockHand.Seconds, ClockHand.Minutes, ClockHand.Hours)
+
+                clockHands.forEach { clockHand ->
+                    val angleInDegrees = when (clockHand) {
+                        ClockHand.Seconds -> {
+                            seconds * 360f / 60f
+                        }
+
+                        ClockHand.Minutes -> {
+                            (minutes + seconds / 60f) * 360f / 60f
+                        }
+
+                        ClockHand.Hours -> {
+                            (((hours % 12f) / 12f * 60) * minutes / 12f) * 360f / 60
+                        }
+                    }
+                    val lineLength = when (clockHand) {
+                        ClockHand.Seconds -> {
+                            circleRadius * 0.8f
+                        }
+
+                        ClockHand.Minutes -> {
+                            circleRadius * 0.7f
+                        }
+
+                        ClockHand.Hours -> {
+                            circleRadius * 0.5f
+                        }
+                    }
+                    val lineThickness = when (clockHand) {
+                        ClockHand.Seconds -> {
+                            3f
+                        }
+
+                        ClockHand.Minutes -> {
+                            7f
+                        }
+
+                        ClockHand.Hours -> {
+                            9f
+                        }
+                    }
+                    val start = Offset(
+                        x = circleCenter.x,
+                        y = circleCenter.y
+                    )
+                    val end = Offset(
+                        x = circleCenter.x,
+                        y = circleCenter.y + lineLength
+                    )
+                    rotate(
+                        angleInDegrees + 180,
+                        pivot = start,
+                        block = {
+                            drawLine(
+                                color = if (clockHand == ClockHand.Seconds){
+                                    redOrange
+                                }else{
+                                    gray
+                                },
+                                start = start,
+                                end = end,
+                                strokeWidth = lineThickness.dp.toPx()
+                            )
+                        }
+                    )
+                }
             }
         )
     }
+}
+
+enum class ClockHand {
+    Seconds, Minutes, Hours
 }
