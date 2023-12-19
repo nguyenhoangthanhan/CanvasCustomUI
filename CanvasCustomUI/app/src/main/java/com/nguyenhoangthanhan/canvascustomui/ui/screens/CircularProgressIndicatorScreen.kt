002cf6ff9d00792aeaa3524e857c7dc91ff7f241
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-19 16:38:43 +0700

    Implemented successfully coding of CustomCustomCircularProgressIndicator

--- CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/CircularProgressIndicatorScreen.kt
+++ CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/CircularProgressIndicatorScreen.kt
@@ -1,19 +1,13 @@
 package com.nguyenhoangthanhan.canvascustomui.ui.screens
 
+import android.graphics.Paint
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
-import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,30 +17,27 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nguyenhoangthanhan.canvascustomui.navigation.CanvasCustomUIRouter
 import com.nguyenhoangthanhan.canvascustomui.navigation.Screen
 import com.nguyenhoangthanhan.canvascustomui.navigation.SystemBackButtonHandler
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.blueGray
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.brightBlue
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.darkGray
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.gray
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.green
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.orange
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.purple
-import com.nguyenhoangthanhan.canvascustomui.ui.theme.redOrange
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.white
-import kotlin.math.roundToInt
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun CircularProgressIndicatorScreen() {
@@ -60,11 +51,9 @@ fun CircularProgressIndicatorScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
-        Button(
-            onClick = {
-                CanvasCustomUIRouter.navigateTo(Screen.HomeScreen)
-            }
-        ) {
+        Button(onClick = {
+            CanvasCustomUIRouter.navigateTo(Screen.HomeScreen)
+        }) {
             Text(text = "CircularProgressIndicatorScreen")
         }
     }
@@ -72,4 +61,131 @@ fun CircularProgressIndicatorScreen() {
     SystemBackButtonHandler {
         CanvasCustomUIRouter.navigateTo(Screen.HomeScreen)
     }
+}
+
+@Composable
+fun CustomCircularProgressIndicator(
+    modifier: Modifier = Modifier,
+    initialValue: Int,
+    primaryColor: Color,
+    secondaryColor: Color,
+    minValue: Int = 0,
+    maxValue: Int = 100,
+    circleRadius: Float,
+    onPositionChange: (Int) -> Unit
+) {
+    var circleCenter by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    var positionValue by remember {
+        mutableStateOf(initialValue)
+    }
+
+    Box(
+        modifier = modifier
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize(),
+            onDraw = {
+                val width = size.width
+                val height = size.height
+                val circleThickness = width / 25f
+                circleCenter = Offset(x = width / 2f, y = height / 2f)
+
+                drawCircle(
+                    brush = Brush.radialGradient(
+                        listOf(
+                            primaryColor.copy(0.45f), secondaryColor.copy(0.15f)
+                        )
+                    ), radius = circleRadius, center = circleCenter
+                )
+
+                drawCircle(
+                    style = Stroke(
+                        width = circleThickness
+                    ), color = secondaryColor, radius = circleRadius, center = circleCenter
+                )
+
+                drawArc(
+                    color = primaryColor,
+                    startAngle = 90f,
+                    sweepAngle = (360f / maxValue) * positionValue.toFloat(),
+                    style = Stroke(
+                        width = circleThickness, cap = StrokeCap.Round
+                    ),
+                    useCenter = false,
+                    size = Size(
+                        width = circleRadius * 2f, height = circleRadius * 2f
+                    ),
+                    topLeft = Offset(
+                        (width - circleRadius * 2f) / 2f, (height - circleRadius * 2f) / 2f
+                    )
+                )
+
+                val outerRadius = circleRadius + circleThickness / 2f
+                val gap = 15f
+                for (i in 0 until (maxValue - minValue)) {
+                    val color = if (i < positionValue - minValue) primaryColor
+                    else primaryColor.copy(alpha = 0.3f)
+                    val angleInDegrees = i * 360 / (maxValue - minValue).toFloat()
+                    val angleInRad = angleInDegrees * PI / 180f + PI / 2f
+
+                    val yGapAdjustment = cos(angleInDegrees * PI / 180f) * gap
+                    val xGapAdjustment = -sin(angleInDegrees * PI / 180f) * gap
+
+                    val start = Offset(
+                        x = (outerRadius * cos(angleInRad) + circleCenter.x + xGapAdjustment).toFloat(),
+                        y = (outerRadius * sin(angleInRad) + circleCenter.y + yGapAdjustment).toFloat()
+                    )
+
+                    val end = Offset(
+                        x = (outerRadius * cos(angleInRad) + circleCenter.x + xGapAdjustment).toFloat(),
+                        y = (outerRadius * sin(angleInRad) + circleThickness + circleCenter.y + yGapAdjustment).toFloat()
+                    )
+
+                    rotate(
+                        angleInDegrees,
+                        pivot = start
+                    ) {
+                        drawLine(
+                            color = color,
+                            start = start,
+                            end = end,
+                            strokeWidth = 1.dp.toPx()
+                        )
+                    }
+                }
+
+                drawContext.canvas.nativeCanvas.apply {
+                    drawIntoCanvas {
+                        drawText(
+                            "$positionValue %",
+                            circleCenter.x,
+                            circleCenter.y + 45.dp.toPx() / 3f,
+                            Paint().apply {
+                                textSize = 38.sp.toPx()
+                                textAlign = Paint.Align.CENTER
+                                color = white.toArgb()
+                                isFakeBoldText = true
+                            }
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun Preview() {
+    CustomCircularProgressIndicator(modifier = Modifier.size(250.dp),
+        initialValue = 1,
+        primaryColor = orange,
+        secondaryColor = gray,
+        circleRadius = 230f,
+        onPositionChange = {
+
+        })
 }
