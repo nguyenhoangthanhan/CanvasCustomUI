10ef779e23353a334919803e7cd7e67c672a2aee
Nguyen Hoang Thanh An - AnNHT2 <skyannguyenhoangthanh@gmail.com>
2023-12-19 18:24:38 +0700

    Coding Android Circular Progress Indicator + Circular Slider in Jetpack Compose completed

--- CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/CircularProgressIndicatorScreen.kt
+++ CanvasCustomUI/app/src/main/java/com/nguyenhoangthanhan/canvascustomui/ui/screens/CircularProgressIndicatorScreen.kt
@@ -2,13 +2,11 @@ package com.nguyenhoangthanhan.canvascustomui.ui.screens
 
 import android.graphics.Paint
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -26,36 +24,45 @@ import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.nguyenhoangthanhan.canvascustomui.navigation.CanvasCustomUIRouter
 import com.nguyenhoangthanhan.canvascustomui.navigation.Screen
 import com.nguyenhoangthanhan.canvascustomui.navigation.SystemBackButtonHandler
+import com.nguyenhoangthanhan.canvascustomui.ui.theme.darkGray
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.gray
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.orange
 import com.nguyenhoangthanhan.canvascustomui.ui.theme.white
 import kotlin.math.PI
+import kotlin.math.atan2
 import kotlin.math.cos
+import kotlin.math.roundToInt
 import kotlin.math.sin
 
 @Composable
 fun CircularProgressIndicatorScreen() {
-    var showDescription by remember {
-        mutableStateOf(false)
-    }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Button(onClick = {
-            CanvasCustomUIRouter.navigateTo(Screen.HomeScreen)
-        }) {
-            Text(text = "CircularProgressIndicatorScreen")
-        }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(darkGray),
+        contentAlignment = Alignment.Center
+    ){
+        CustomCircularProgressIndicator(
+            modifier = Modifier
+                .size(250.dp)
+                .background(darkGray)
+            ,
+            initialValue = 50,
+            primaryColor = orange,
+            secondaryColor = gray,
+            circleRadius = 230f,
+            onPositionChange = { position ->
+                //do something with this position value
+            }
+        )
     }
 
     SystemBackButtonHandler {
@@ -82,11 +89,57 @@ fun CustomCircularProgressIndicator(
         mutableStateOf(initialValue)
     }
 
+    var changeAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var dragStartedAngle by remember {
+        mutableStateOf(0f)
+    }
+
+    var oldPositionValue by remember {
+        mutableStateOf(initialValue)
+    }
+
     Box(
         modifier = modifier
     ) {
         Canvas(
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(true) {
+                    detectDragGestures(
+                        onDragStart = { offset ->
+                            dragStartedAngle = -atan2(
+                                x = circleCenter.y - offset.y,
+                                y = circleCenter.x - offset.x
+                            ) * (180f / PI).toFloat()
+                            dragStartedAngle = (dragStartedAngle + 180f).mod(360f)
+                        },
+                        onDrag = { change, _ ->
+                            var touchAngle = -atan2(
+                                x = circleCenter.y - change.position.y,
+                                y = circleCenter.x - change.position.x
+                            ) * (180f / PI).toFloat()
+                            touchAngle = (touchAngle + 180f).mod(360f)
+
+                            val currentAngle = oldPositionValue * 360f / (maxValue - minValue)
+                            changeAngle = touchAngle - currentAngle
+
+                            val lowerThreshold = currentAngle - (360f / (maxValue - minValue) * 5)
+                            val higherThreshold = currentAngle + (360f / (maxValue - minValue) * 5)
+
+                            if (dragStartedAngle in lowerThreshold..higherThreshold) {
+                                positionValue =
+                                    (oldPositionValue + (changeAngle / (360f / (maxValue - minValue))).roundToInt())
+                            }
+                        },
+                        onDragEnd = {
+                            oldPositionValue = positionValue
+                            onPositionChange(positionValue)
+                        }
+                    )
+                },
             onDraw = {
                 val width = size.width
                 val height = size.height
@@ -98,13 +151,17 @@ fun CustomCircularProgressIndicator(
                         listOf(
                             primaryColor.copy(0.45f), secondaryColor.copy(0.15f)
                         )
-                    ), radius = circleRadius, center = circleCenter
+                    ),
+                    radius = circleRadius,
+                    center = circleCenter
                 )
 
                 drawCircle(
                     style = Stroke(
                         width = circleThickness
-                    ), color = secondaryColor, radius = circleRadius, center = circleCenter
+                    ), color = secondaryColor,
+                    radius = circleRadius,
+                    center = circleCenter
                 )
 
                 drawArc(
